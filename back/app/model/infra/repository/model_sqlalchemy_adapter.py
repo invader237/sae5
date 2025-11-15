8667110828a381efc09f8a6855904b482e8cf438
invader237 <diego.trivino.fr@gmail.com>
2025-11-15 16:39:41 +0100

    :sparkles:(model): implement model activation endpoint

--- back/app/model/infra/repository/model_sqlalchemy_adapter.py
+++ back/app/model/infra/repository/model_sqlalchemy_adapter.py
@@ -1,5 +1,6 @@
 from app.model.domain.catalog.model_catalog import ModelCatalog
 from app.model.infra.repository.model_repository import ModelRepository
+from app.model.domain.entity.model import Model
 
 
 class ModelSQLAlchemyAdapter(ModelCatalog):
@@ -9,5 +10,11 @@ class ModelSQLAlchemyAdapter(ModelCatalog):
     def find_all(self):
         return self.repository.find_all()
 
-    def save(self, model_in: dict):
-        return self.repository.save(model_in)
+    def find_by_id(self, model_id: str):
+        return self.repository.find_by_id(model_id)
+
+    def find_active_model(self):
+        return self.repository.find_active_model()
+
+    def save(self, model: Model):
+        return self.repository.save(model)
