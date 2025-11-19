4387957dee7552670063aeef4ac6019e8be6a343
invader237 <diego.trivino.fr@gmail.com>
2025-11-19 17:25:02 +0100

    :rotating_light:(main.py): fix linter warning

--- back/app/main.py
+++ back/app/main.py
@@ -14,10 +14,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8081"],       # Liste des domaines autorisés
+    allow_origins=["http://localhost:8081"],  # Liste des domaines autorisés
     allow_credentials=True,
-    allow_methods=["*"],         # GET, POST, PUT, DELETE, etc.
-    allow_headers=["*"],         # Autoriser tous les headers
+    allow_methods=["*"],  # GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],  # Autoriser tous les headers
 )
 
 
