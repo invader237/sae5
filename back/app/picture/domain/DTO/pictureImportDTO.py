ba3f63d995b61b6db7d7fed3072a1a52033de57f
mfries <mathias.fries4@gmail.com>
2025-11-21 09:57:12 +0100

    :bug:(fix): fix upload picture dto

--- back/app/picture/domain/DTO/pictureImportDTO.py
+++ back/app/picture/domain/DTO/pictureImportDTO.py
@@ -1,8 +1,6 @@
 from pydantic import BaseModel
 from typing import Optional
-from uuid import UUID
 from typing import Literal
 
 class PictureImportDTO(BaseModel):
     sendType: Optional[Literal["analyse", "database"]] = None
-
