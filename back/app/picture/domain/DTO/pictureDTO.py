7974cce0c142caf51fd3f4b8c41649f553cb92ed
Joshua Brochot <150423542+JoshuaBrochot@users.noreply.github.com>
2025-11-24 19:09:16 +0100

    🏗️(DTO): Refactor DTOs and update import paths
    Moved PictureDTO and UserDTO to their respective domain/DTO directories and updated all related import statements. Removed UserDTO from pictureDTO.py and created a dedicated userDTO.py. This improves code organization and separation of concerns.

--- back/app/picture/domain/DTO/pictureDTO.py
+++ back/app/picture/domain/DTO/pictureDTO.py
@@ -1,8 +1,16 @@
-from pydantic import BaseModel
+from __future__ import annotations
+
 from typing import Optional
 from uuid import UUID
+from datetime import datetime
+
+from pydantic import BaseModel
 
 
 class PictureDTO(BaseModel):
     id: Optional[UUID] = None
     path: Optional[str] = None
+    analyse_by: Optional[str] = None
+    pourcentage: Optional[float] = None
+    date_detection: Optional[datetime] = None
+    date_validation: Optional[datetime] = None
