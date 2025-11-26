507d7c18eed73411f51f4000537a179caec65696
mfries <mathias.fries4@gmail.com>
2025-11-26 20:17:17 +0100

    :art:(front): API url structure optimization

--- front/api/axiosConfig.ts
+++ front/api/axiosConfig.ts
@@ -1,7 +1,13 @@
 import axios from 'axios';
 import Constants from 'expo-constants';
 
-const baseURL = Constants.expoConfig?.extra?.backendApiAddress || process.env.EXPO_PUBLIC_BACKEND_API_ADDRESS;
+const envBackend =
+  typeof globalThis !== 'undefined' && 'process' in globalThis
+    ? (globalThis as any).process?.env?.EXPO_PUBLIC_BACKEND_API_ADDRESS
+    : undefined;
+
+export const baseURL =
+  Constants.expoConfig?.extra?.backendApiAddress || envBackend;
 
 const axiosInstance = axios.create({
   baseURL
