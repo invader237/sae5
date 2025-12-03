f532031fc771d1c7fa7206bb42e71c018772542c
invader237 <diego.trivino.fr@gmail.com>
2025-12-03 21:55:45 +0100

    :sparkles:(pva): add endpoint to retrive picture that need validation for pva

---
+++ back/app/picture/domain/mapper/picture_to_picturePvaDTO_mapper.py
@@ -0,0 +1,22 @@
+from app.picture.domain.DTO.picturePvaDTO import PicturePvaDTO
+from app.picture.domain.entity.picture import Picture
+from app.room.domain.DTO.roomLightDTO import RoomLightDTO
+from app.room.domain.mapper.room_to_roomLightDTO_mapper import (
+    room_to_roomLightDTO_mapper,
+)
+
+
+class PictureToPicturePvaDTOMapper:
+    @staticmethod
+    def apply(picture: Picture) -> PicturePvaDTO:
+        return PicturePvaDTO(
+            id=picture.image_id,
+            path=picture.path,
+            recognition_percentage=getattr(
+                picture, "recognition_percentage", None
+                ),
+            room=room_to_roomLightDTO_mapper.apply(picture.room)
+        )
+
+
+picture_to_picturePvaDTO_mapper = PictureToPicturePvaDTOMapper()
