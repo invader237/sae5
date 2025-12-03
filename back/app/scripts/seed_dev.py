7153fbf07a7dc9075494d385e4c4009273de42a4
invader237 <diego.trivino.fr@gmail.com>
2025-12-03 21:51:27 +0100

    :bug:(picture): fix picture to use entity insteade of dict for data transit

--- back/app/scripts/seed_dev.py
+++ back/app/scripts/seed_dev.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app.database import SessionLocal
 from app.picture.infra.repository.picture_repository import PictureRepository
 from app.room.infra.repository.room_repository import RoomRepository
@@ -6,6 +7,7 @@ from app.model.infra.repository.model_repository import ModelRepository
 from app.model.domain.entity.model import Model
 from app.user.domain.entity.user import User
 from app.authentification.auth_utils import hash_password
+from app.picture.domain.entity.picture import Picture
 
 
 def load_fixtures() -> None:
@@ -34,6 +36,37 @@ def load_fixtures() -> None:
             "departement": "GEA",
             "type": "normal",
         })
+        room_repo.save({
+            "name": "A321",
+            "floor": 3,
+            "departement": "INFO",
+            "type": "normal",
+        })
+
+        # Pictures
+        picture_repo.save(
+            Picture(
+                path="/images/room_f36_view1.png",
+                analyzed_by="model_v1",
+                recognition_percentage=95.0,
+                analyse_date=datetime(2024, 1, 15, 10, 0, 0),
+                validation_date=datetime(2024, 1, 15, 12, 0, 0),
+                is_validated=True,
+                room=room_repo.find_by_name("F36"),
+            )
+        )
+
+        picture_repo.save(
+            Picture(
+                path="/images/room_e23_view2.png",
+                analyzed_by="model_v1",
+                recognition_percentage=88.5,
+                analyse_date=datetime(2024, 1, 16, 11, 30, 0),
+                validation_date=None,
+                is_validated=False,
+                room=room_repo.find_by_name("E23"),
+            )
+        )
 
         # ---------- ROLES ----------
         role_repo.save({"type": "admin"})
