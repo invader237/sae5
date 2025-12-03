c5666c2ad2024254f2235eb8fe57b8616f8cd28d
invader237 <diego.trivino.fr@gmail.com>
2025-12-03 21:51:27 +0100

    :bug:(picture): fix picture to use entity insteade of dict for data transit

--- back/app/picture/infra/repository/picture_sqlalchemy_adapter.py
+++ back/app/picture/infra/repository/picture_sqlalchemy_adapter.py
@@ -1,5 +1,6 @@
 from typing import Union
 from uuid import UUID
+from app.picture.domain.entity.picture import Picture
 from app.picture.domain.catalog.picture_catalog import PictureCatalog
 from app.picture.infra.repository.picture_repository import PictureRepository
 
@@ -11,11 +12,14 @@ class PictureSQLAlchemyAdapter(PictureCatalog):
     def find_all(self):
         return self.repository.find_all()
 
-    def save(self, picture_in: dict):
-        return self.repository.save(picture_in)
+    def save(self, picture: Picture):
+        return self.repository.save(picture)
 
     def update(self, picture_id: Union[str, UUID], updates: dict):
         return self.repository.update(picture_id, updates)
 
     def find_by_id(self, picture_id: Union[str, UUID]):
         return self.repository.find_by_id(picture_id)
+
+    def find_by_not_validated(self):
+        return self.repository.find_by_not_validated()
