d1b219883675f8524070eeef85ac84375db277f3
invader237 <diego.trivino.fr@gmail.com>
2025-12-11 00:42:54 +0100

    :sparkles:(room): add new room api endpoint

--- front/api/room.api.ts
+++ front/api/room.api.ts
@@ -1,5 +1,6 @@
 import axiosInstance from './axiosConfig';
 import RoomLightDTO from './DTO/roomLight.dto';
+import RoomDTO from './DTO/room.dto';
 
 export const fetchRoomsForPva = async (): Promise<RoomLightDTO[]> => {
   try {
@@ -10,3 +11,23 @@ export const fetchRoomsForPva = async (): Promise<RoomLightDTO[]> => {
     throw error;
   }
 };
+
+export const saveRoom = async (room: RoomDTO): Promise<null> => {
+    try {
+        const response = await axiosInstance.post('/rooms', room);
+        return response.data;
+    } catch (error) {
+        console.error('Error adding room:', error);
+        throw error;
+    }
+}
+
+export const getRooms = async (): Promise<RoomDTO[]> => {
+    try {
+        const response = await axiosInstance.get('/rooms');
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching rooms:', error);
+        throw error;
+    }
+}
