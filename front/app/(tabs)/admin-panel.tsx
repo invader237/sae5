99bf709a69c0c0569da6189075c418b1edaadbfc
invader237 <diego.trivino.fr@gmail.com>
2025-12-10 21:40:36 +0100

    :lipstick:(admin-panel): makes the admin panel scrollable

--- front/app/(tabs)/admin-panel.tsx
+++ front/app/(tabs)/admin-panel.tsx
@@ -1,15 +1,22 @@
-import { View, Text } from "react-native";
-import ModelSelector from "../../components/model-selector";
-import PvaPanel from "../../components/pva-panel";
+import { View, Text, ScrollView } from "react-native";
+import ModelSelector from "@/components/model-selector";
+import PvaPanel from "@/components/pva-panel";
+import RoomManagmentPanel from "@/components/room-managment-panel";
 
 export default function AdminPanel() {
   return (
-    <View className="flex-1 bg-white p-6 gap-4">
-      <Text className="text-[24px] font-bold text-[#007bff]">Panneau Admin</Text>
+    <ScrollView className="flex-1 bg-white p-6 gap-4">
+      <View className="gap-4">
+      <Text className="text-[24px] font-bold text-[#007bff] mb-4">Panneau Admin</Text>
 
       <ModelSelector />
 
       <PvaPanel />
-    </View>
+
+      <RoomManagmentPanel />
+
+      <View className="h-[200px]" />
+      </View>
+    </ScrollView>
   );
 }
