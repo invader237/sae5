18bdbc30677993bb36653edb3c2b630c7466d74f
Mathias <mathias.fries4@gmail.com>
2025-12-12 15:07:28 +0100

    (app): wrap root layout with AuthProvider

---
+++ front/app/_layout.tsx
@@ -0,0 +1,14 @@
+import { Stack } from "expo-router";
+import { AuthProvider } from "@/contexts/AuthContext";
+import "../global.css";
+
+export default function RootLayout() {
+  return (
+    <AuthProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+      </Stack>
+    </AuthProvider>
+  );
+}
