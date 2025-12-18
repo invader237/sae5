0c3a1068f54c69f30d8c345abc825e3fbfe5fc89
Mathias <mathias.fries4@gmail.com>
2025-12-19 00:04:46 +0100

    :recycle:(history): Refactor code and avoid redundancy

--- back/app/model/domain/DTO/modelLightDTO.py
+++ back/app/model/domain/DTO/modelLightDTO.py
@@ -1,10 +1,9 @@
+from typing import Optional
 from pydantic import BaseModel
 from uuid import UUID
 
 
 class ModelLightDTO(BaseModel):
-    id: UUID | None = None
-    name: str | None = None
+    id: Optional[UUID] = None
+    name: Optional[str] = None
 
-    class Config:
-        from_attributes = True
