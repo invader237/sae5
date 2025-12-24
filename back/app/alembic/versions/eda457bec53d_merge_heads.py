7c5fd9323f21d86260273e9e5bb899015ec61c88
invader237 <diego.trivino.fr@gmail.com>
2025-12-24 19:02:09 +0100

    :rotating_light:(db): fix linter warning in migration script

--- back/app/alembic/versions/eda457bec53d_merge_heads.py
+++ back/app/alembic/versions/eda457bec53d_merge_heads.py
@@ -7,13 +7,13 @@ Create Date: 2025-12-20 16:22:35.294058
 """
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
 
 # revision identifiers, used by Alembic.
 revision: str = 'eda457bec53d'
-down_revision: Union[str, Sequence[str], None] = ('ec05e8989857', '2b6e9f1c4d2a')
+down_revision: Union[str, Sequence[str], None] = (
+    "ec05e8989857",
+    "2b6e9f1c4d2a",
+)
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
