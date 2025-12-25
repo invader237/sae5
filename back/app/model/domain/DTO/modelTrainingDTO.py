e124df029cb23ea53eda90a165e8a0908c66e2ce
invader237 <diego.trivino.fr@gmail.com>
2025-12-25 20:52:35 +0100

    :sparkles:(modelTrainingDTO): add room list

--- back/app/model/domain/DTO/modelTrainingDTO.py
+++ back/app/model/domain/DTO/modelTrainingDTO.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel
 from typing import Literal
-
+from room.domain.DTO.roomLightDTO import RoomLightDTO
 
 class ModelTrainingDTO(BaseModel):
     type: Literal["base", "scratch"]
     epochs: int
     batchSize: int
     learningRate: float
+    roomList: list[RoomLightDTO]
