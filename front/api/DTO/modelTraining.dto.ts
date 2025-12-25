acca45bd043bf723d44ea31a1a1a776431373ff0
invader237 <diego.trivino.fr@gmail.com>
2025-12-25 20:52:35 +0100

    :sparkles:(modelTrainingDTO): add room list

--- front/api/DTO/modelTraining.dto.ts
+++ front/api/DTO/modelTraining.dto.ts
@@ -1,8 +1,10 @@
+import RoomLightDTO from "./roomLight.dto";
 
 export default interface ModelTrainingDTO {
   type: "base" | "scratch";
   epochs: number;
   batchSize: number;
   learningRate: number;
+  roomList: RoomLightDTO[];
 }
 
