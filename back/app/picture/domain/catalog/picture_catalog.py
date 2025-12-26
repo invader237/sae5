866634ae7e959436af5ab04e1f1c82ca03a1eb25
invader237 <diego.trivino.fr@gmail.com>
2025-12-26 20:54:07 +0100

    :beers:(model-training-v2): add room specific training wip

--- back/app/picture/domain/catalog/picture_catalog.py
+++ back/app/picture/domain/catalog/picture_catalog.py
@@ -1,12 +1,18 @@
 from typing import Protocol, Collection, Union
 from uuid import UUID
 from app.picture.domain.entity.picture import Picture
+from app.room.domain.entity.room import Room
 
 
 class PictureCatalog(Protocol):
     def find_all(self) -> Collection[Picture]: ...
     def save(self, picture: Picture) -> None: ...
     def find_by_id(self, picture_id: Union[str, UUID]) -> Picture: ...
+
+    def find_all_validated_by_room_ids(
+        self,
+        rooms: Collection[Room],
+    ) -> Collection[Picture]: ...
 
     def find_by_not_validated(
         self,
