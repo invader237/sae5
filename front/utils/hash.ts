4369947bce2ac239c31d4f549975d0930cae3f6e
FRIES Mathias <mathias.fries4@gmail.com>
2025-12-26 14:43:35 +0100

    :rotating_light:(fix): fix lint

--- front/utils/hash.ts
+++ front/utils/hash.ts
@@ -1,8 +1,8 @@
 import * as Crypto from "expo-crypto";
 
-/**
- * // Hache un mot de passe avec SHA-256 avant envoi à l'API. Le hash est retourné en hexadécimal.
- */
+
+// Hache un mot de passe avec SHA-256 avant envoi à l'API. Le hash est retourné en hexadécimal.
+
 export async function hashPassword(password: string): Promise<string> {
   const hash = await Crypto.digestStringAsync(
     Crypto.CryptoDigestAlgorithm.SHA256,
