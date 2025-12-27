bb2e9dbd140870a398fba5e0952f30972158cae6
invader237 <diego.trivino.fr@gmail.com>
2025-12-27 18:02:04 +0100

    :rotating_light:(model-training): fix linter warning

--- back/app/model/domain/DTO/modelTrainingDTO.py
+++ back/app/model/domain/DTO/modelTrainingDTO.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from typing import Literal
 from app.room.domain.DTO.roomLightDTO import RoomLightDTO
 
+
 class ModelTrainingDTO(BaseModel):
     type: Literal["base", "scratch"]
     epochs: int
