3b320377f169b5ad08b6c2bbfc154e4eaedb216f
invader237 <diego.trivino.fr@gmail.com>
2025-12-30 22:51:37 +0100

    :sparkles:(model-training): add model namming engine

--- back/app/model/infra/factory/model_factory.py
+++ back/app/model/infra/factory/model_factory.py
@@ -14,6 +14,7 @@ from app.room.domain.catalog.room_catalog import RoomCatalog
 from app.picture.domain.catalog.picture_catalog import PictureCatalog
 from app.picture.infra.factory.picture_factory import get_picture_catalog
 from app.database import get_session
+from app.model.domain.service.model_namer import ModelNamer
 
 
 def get_model_catalog(db: Session = Depends(get_session)) -> ModelCatalog:
@@ -29,14 +30,20 @@ def get_model_loader(
         model_catalog=model_catalog, models_dir="/app/models"
         )
 
+def get_model_namer(model_catalog: ModelCatalog = Depends(get_model_catalog)) -> ModelNamer:
+    return ModelNamer(model_catalog)
+
 
 def get_model_training(
     room_catalog: RoomCatalog = Depends(get_room_catalog),
     model_catalog: ModelCatalog = Depends(get_model_catalog),
     picture_catalog: PictureCatalog = Depends(get_picture_catalog),
+    model_namer: ModelNamer = Depends(get_model_namer)
 ) -> ModelTraining:
     return ModelTraining(
         room_catalog=room_catalog,
         model_catalog=model_catalog,
         picture_catalog=picture_catalog,
+        model_namer=model_namer
     )
+
